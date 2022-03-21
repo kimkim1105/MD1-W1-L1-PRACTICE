66aecdd16b141b07f7cc66e50fb2f80e0998ba1d
unknown <huenk105@gmail.com>
2022-03-21 17:25:16 +0700

    demo

---
+++ is-leap-year/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int year;
+        System.out.println("Enter a year: ");
+        year = scanner.nextInt();
+        boolean isLeapYear = false;
+
+        if(year%4==0){
+            if(year%100==0){
+                if(year%400==0){
+                    isLeapYear = true;
+                }
+            }else {isLeapYear = true;}
+        }
+        if(isLeapYear){
+            System.out.printf("%d is a leap year",year);;
+        }else {
+            System.out.printf("%d is not a leap year",year);
+        }
+    }
+}
